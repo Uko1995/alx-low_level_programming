fda7c9440f7ce12c626b10a7371b1cd81b653ca0
Uko Uwatt <uwattuko@gmail.com>
2023-06-20 05:23:38 +0000

    task 0

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
 
 int _putchar(char c);
 int _islower(int c);
